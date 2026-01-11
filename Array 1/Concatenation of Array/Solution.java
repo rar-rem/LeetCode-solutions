f030f3df121f6768ee2fb25a8d2b0f66591961e4
Remya A Reji <77137214+rar-rem@users.noreply.github.com>
2026-01-10 23:05:14 -0600

    Update Solution.java

--- Array 1/Concatenation of Array/Solution.java
+++ Array 1/Concatenation of Array/Solution.java
@@ -3,22 +3,41 @@ Problem: Concatenation of Array
 LeetCode #: 1929
 Difficulty: Easy
 Topic: Array
+
+Approach:
+- Find the length of the input array.
+- Create a new array of size 2 * length.
+- Iterate from index 0 to 2 * length - 1.
+- For the first half, copy elements directly from nums.
+- For the second half, copy elements from nums using (i - length).
+
+Time Complexity: O(n)
+Space Complexity: O(n)
 */
 
 class Solution {
     public int[] getConcatenation(int[] nums) {
 
+        // Length of the original array
         int len = nums.length;
+
+        // Create a new array of size 2 * len
         int[] ans = new int[2 * len];
 
+        // Loop through the new array
         for (int i = 0; i < 2 * len; i++) {
+
+            // If index is in the first half, copy directly
             if (i < len) {
                 ans[i] = nums[i];
-            } else {
+            }
+            // If index is in the second half, copy using (i - len)
+            else {
                 ans[i] = nums[i - len];
             }
         }
 
+        // Return the concatenated array
         return ans;
     }
 }
